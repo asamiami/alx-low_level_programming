7f01f560be05d5cc0d20c9cca14d0dc25cf7ac89
Nwasike Vivian <vnwasike@gmail.com>
2022-09-10 19:28:29 -0700

    Runable

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,10 @@
 #!/bin/bash
-puts("\"Programming is like building a mutilingual puzzle");
+#include<stdio.h>
+/*main*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
+}
+
