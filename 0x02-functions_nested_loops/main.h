61f9f0d0be171fe711e2a4f1a1e406c83ad1a007
Nwasike Vivian <vnwasike@gmail.com>
2022-09-13 14:50:41 -0700

    Put char

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,16 @@
+#ifdef HOLBERTON_H_INCLUDED
+#define HOLBERTON_H_INCLUDED
+
+int _putchar(char);
+void print_alphabet(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+void print_last_digit(int);
+void jack_bawer(void);
+int add(int a, int b);
+void print_to_98(int n);
+void print_times_tables(int n);
+voi num(int r, int n);
+
+#endif
