0f2da9f9417e9720b331f3280b6b6a78fdbd0bd2
Nwasike Vivian <vnwasike@gmail.com>
2022-09-13 16:17:37 -0700

    Is Alpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -7,21 +7,10 @@
  * letter, lowercase or uppercase.
  * Return: 1 or 0 in otherwise.
  */
+
 int _isalpha(int c)
 {
-	char lower, upper;
-	int isletter = 0;
 
-	for (lower = 'a'; lower <= 'z'; lower++)
-	{
-		for (upper = 'A'; upper <= 'Z'; upper++)
-		{
-			if (c == lower || c == upper)
-			{
-				isletter = 1;
-			}
-		}
-	}
-	return (isletter);
+	return ((c >= 'a' && c <= ;'z') || (c >= 'A' && c <= 'Z'));
 }
 
